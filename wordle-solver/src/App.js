bf1b13ae2f3001944ed853583b89158561f8ab01
Andrew.Segal <andrew.segal@moz.com>
2022-04-04 00:40:51 -0400

    Add styled components, add basic html structure

--- wordle-solver/src/App.js
+++ wordle-solver/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Game from './components/game.js';
+
+const Title = styled.h1`
+  font-family: "nyt-karnak";
+  font-weight: 700;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title>WordleSolver</Title>
       </header>
+      <main>
+        <Game />
+      </main>
     </div>
   );
 }
---
+++ wordle-solver/src/components/game.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Game = () => {
+    return <div>heyo bud</div>
+}
+
+export default Game
